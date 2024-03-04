876937fb96384292402e9bdb3185f3451a771ccc
Jan Pawellek <2392542+janpawellek@users.noreply.github.com>
2024-03-04 15:07:14 +0100

    Fix login page background

--- app/app/(auth)/layout.tsx
+++ app/app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="flex min-h-screen flex-col justify-center bg-muted/50 py-12 sm:px-6 lg:px-8">
       {children}
     </div>
   );
--- app/app/(auth)/login/page.tsx
+++ app/app/(auth)/login/page.tsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
   const error = searchParams?.get("error");
 
   return (
-    <div className="mx-5 border border-stone-200 py-10 dark:border-stone-700 sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg sm:shadow-md">
+    <div className="mx-5 border border-stone-200 bg-background py-10 dark:border-stone-700 sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg sm:shadow-md">
       <Image
         alt="ownAI"
         width={64}
