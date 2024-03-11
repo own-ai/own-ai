4ceb989e4d960f69a886bc27acdecebb273718b2
Jan Pawellek <2392542+janpawellek@users.noreply.github.com>
2024-03-11 14:07:01 +0100

    Fix auth cookie for custom domains

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,43 @@
-import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
+import { NextRequest } from "next/server";
+import { authOptions } from "@/lib/auth";
 
-const handler = NextAuth(authOptions);
+// Handler for custom domains.
+// We don't set the cookie domain to avoid it being rejected.
+const theirHandler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// If the user signs in to ownai.com or any subdomain.ownai.com,
+// set the cookie domain in order to apply it and the login state to all subdomains.
+// This allows the user to sign in only once for all subdomains.
+const ourHandler = NextAuth({
+  ...authOptions,
+  cookies: {
+    ...authOptions.cookies,
+    sessionToken: {
+      ...authOptions.cookies!.sessionToken!,
+      options: {
+        ...authOptions.cookies!.sessionToken!.options,
+        domain: `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`,
+      },
+    },
+  },
+});
 
 // The edge runtime needs next-auth 5
 // export const runtime = "edge";
+
+function handle(...args: any[]) {
+  const request: NextRequest = args[0];
+  const host = request.headers.get("host");
+
+  if (
+    host === process.env.NEXT_PUBLIC_ROOT_DOMAIN ||
+    host?.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`)
+  ) {
+    return ourHandler(...args);
+  }
+
+  return theirHandler(...args);
+}
+
+export { handle as GET, handle as POST };
--- lib/auth.ts
+++ lib/auth.ts
@@ -34,10 +34,6 @@ export const authOptions: NextAuthOptions = {
         httpOnly: true,
         sameSite: "lax",
         path: "/",
-        // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT
-          ? `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
-          : undefined,
         secure: VERCEL_DEPLOYMENT,
       },
     },
