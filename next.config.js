b1c91f034c9f3f51cd0b842f9c79e8cbee6d73a0
Jan Pawellek <2392542+janpawellek@users.noreply.github.com>
2024-08-01 15:01:01 +0200

    Add Content Security Policy header

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data:;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+`;
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -24,4 +36,17 @@ module.exports = {
     ],
   },
   output: process.env.BUILD_STANDALONE === "1" ? "standalone" : undefined,
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
 };
